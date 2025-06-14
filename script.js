9ba3666028d4195450680cd3e6ac72ee010fa8ce
matheusricardooooo <matheusricardodev@gmail.com>
2025-06-14 12:47:26 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+    function showSidebar() {
+        const sidebar = document.querySelector(".sidebar");
+        sidebar.style.display = "flex";
+}
+
+    function hideSidebar() {
+        const sidebar = document.querySelector(".sidebar");
+        sidebar.style.display = "none";
+}
+
+    function contact() {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth"})
+}
